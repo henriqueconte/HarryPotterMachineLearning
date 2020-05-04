d41553d39f615a0885b22acf80809b87e20251d9
henriqueconte <henriqueconte6@gmail.com>
2020-05-04 16:26:21 -0300

    Implemented person detector

--- HarryPotterMachineLearning/ViewController.swift
+++ HarryPotterMachineLearning/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     let shareWithFriends = ShareWithFriends()
-    
+    let mlController = MLController()
 
     @IBOutlet weak var class3ValuePredict: UILabel!
     @IBOutlet weak var class2ValuePredict: UILabel!
@@ -43,34 +43,49 @@ class ViewController: UIViewController {
         class3ValuePredict.isHidden = true
     }
     
+    func detectHPCharacter(image: UIImage) {
+        let predicts = mlController.predictHPCharacter(image: image).filter { $0.valuePredict >= 0.01
+        }
+        for i in 0 ..< (predicts.count < 3 ? predicts.count : 3) {
+            switch i {
+            case 0:
+                self.class1Label.text = predicts[i].name
+                self.class1Label.isHidden = false
+                self.class1ValuePredict.text = Int(predicts[i].valuePredict * 100).description
+                self.class1ValuePredict.isHidden = false
+            case 1:
+                self.class2Label.text = predicts[i].name
+                self.class2Label.isHidden = false
+                self.class2ValuePredict.text = Int(predicts[i].valuePredict * 100).description
+                self.class2ValuePredict.isHidden = false
+            case 3:
+                self.class3Label.text = predicts[i].name
+                self.class3Label.isHidden = false
+                self.class3ValuePredict.text = Int(predicts[i].valuePredict * 100).description
+                self.class3ValuePredict.isHidden = false
+            default:
+                break
+            }
+        }
+    }
+    
 }
 
 extension ViewController: ImageControllerDelegate {
     func didSelect(image: UIImage?) {
         DispatchQueue.main.async {
             self.imageView.image = image
-            let mlController = MLController()
-            let predicts = mlController.predict(image: image).filter { $0.valuePredict >= 0.01
+            
+            let predicts = self.mlController.predictPerson(image: image).filter { $0.valuePredict >= 0.01
             }
-            for i in 0 ..< (predicts.count < 3 ? predicts.count : 3) {
-                switch i {
-                case 0:
-                    self.class1Label.text = predicts[i].name
+            
+            if predicts.count > 0 {
+                if predicts[0].name == "People" {
+                    self.detectHPCharacter(image: image ?? UIImage())
+                }
+                else if predicts[0].name == "Things" {
+                    self.class1Label.text = "Not found"
                     self.class1Label.isHidden = false
-                    self.class1ValuePredict.text = Int(predicts[i].valuePredict * 100).description
-                    self.class1ValuePredict.isHidden = false
-                case 1:
-                    self.class2Label.text = predicts[i].name
-                    self.class2Label.isHidden = false
-                    self.class2ValuePredict.text = Int(predicts[i].valuePredict * 100).description
-                    self.class2ValuePredict.isHidden = false
-                case 3:
-                    self.class3Label.text = predicts[i].name
-                    self.class3Label.isHidden = false
-                    self.class3ValuePredict.text = Int(predicts[i].valuePredict * 100).description
-                    self.class3ValuePredict.isHidden = false
-                default:
-                    break
                 }
             }
         }
