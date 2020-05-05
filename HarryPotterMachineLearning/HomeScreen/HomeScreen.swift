114aaf16d57889dbdc3f56a719a1bd14c347607d
henriqueconte <henriqueconte6@gmail.com>
2020-05-05 15:26:09 -0300

    Implemented home screen without fonts

---
+++ HarryPotterMachineLearning/HomeScreen/HomeScreen.swift
@@ -0,0 +1,19 @@
+//
+//  HomeScreen.swift
+//  HarryPotterMachineLearning
+//
+//  Created by Henrique Figueiredo Conte on 05/05/20.
+//  Copyright © 2020 Henrique Figueiredo Conte. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class HomeScreen: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
